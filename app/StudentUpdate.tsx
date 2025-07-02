a3df7bb4eaa09c0f5f7921714ed65ccf3cfa865b
Alani158 <mujeebalani4@gmail.com>
2025-07-02 13:15:34 +0100

    second commit

---
+++ app/StudentUpdate.tsx
@@ -0,0 +1,78 @@
+import Rectangle from "./images/Rectangle40196.png";
+import image2 from "./images/image2.png";
+import image3 from "./images/image3.png";
+
+export default function StudentUpdate() {
+  return (
+    <div className=" w-full mt-4">
+      <div className="bg-[#FFF6F5] pb-10  mx-auto w-[96%] border-[#FE5E55] border-[1px]">
+        <h1 className="text-center text-4xl">Student Updates</h1>
+        <div className="flex gap-10  w-full flex-col mt-4 ">
+          <div className="sm:flex justify-center gap-10   w-[80%] mx-auto px-10">
+            <div className="sm:w-[30%] w-[100%]">
+              <img
+                src={Rectangle.src}
+                className="w-[100%] sm:h-[400px] h-[200px]"
+                alt="student1IMG"
+              />
+            </div>
+            <div className="sm:w-[50%]">
+              {" "}
+              <p className="sm:text-2xl text-sm mt-4 text-[#4F4F4F]">
+                School Updates Emily's Adventure in SpainHigh school student
+                Emily embarked on a six-month adventure in Spain, delving deep
+                into its culture, language, and history. Dive into her story to
+                uncover her experiences and learnings.{" "}
+              </p>
+              <button className="bg-[#FE5E55] rounded-lg px-2 py-4 text-sm sm:text-2xl text-white w-[80%] sm:w-[50%] mt-6">
+                <p className="text-center">DISCOVER MORE</p>
+              </button>
+            </div>
+          </div>
+          <div className="sm:flex justify-center gap-10 mt-4  w-[80%] mx-auto px-10">
+            <div className="sm:w-[30%] w-[100%]">
+              <img
+                src={image2.src}
+                className="w-[100%] sm:h-[400px] h-[200px]"
+                alt="student1IMG"
+              />
+            </div>
+            <div className="mt-4 sm:w-[50%]">
+              {" "}
+              <p className="sm:text-2xl text-sm text-[#4F4F4F]">
+                School Updates Emily's Adventure in SpainHigh school student
+                Emily embarked on a six-month adventure in Spain, delving deep
+                into its culture, language, and history. Dive into her story to
+                uncover her experiences and learnings.{" "}
+              </p>
+              <button className="bg-[#FE5E55] rounded-lg px-2 py-4 text-sm sm:text-2xl text-white w-[80%] sm:w-[50%] mt-6">
+                <p className="text-center">DISCOVER MORE</p>
+              </button>
+            </div>
+          </div>
+          <div className="sm:flex justify-center gap-10 mt-4  w-[80%] mx-auto px-10">
+            <div className="sm:w-[30%] w-[100%]">
+              <img
+                src={image3.src}
+                className="w-[100%] sm:h-[400px] h-[200px]"
+                alt="student1IMG"
+              />
+            </div>
+            <div className="mt-4 sm:w-[50%]">
+              {" "}
+              <p className="sm:text-2xl text-sm text-[#4F4F4F]">
+                School Updates Emily's Adventure in SpainHigh school student
+                Emily embarked on a six-month adventure in Spain, delving deep
+                into its culture, language, and history. Dive into her story to
+                uncover her experiences and learnings.{" "}
+              </p>
+              <button className="bg-[#FE5E55] rounded-lg px-2 py-4 text-sm sm:text-2xl text-white w-[80%] sm:w-[50%] mt-6">
+                <p className="text-center">DISCOVER MORE</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,60 @@
+import Footer from "./Footer";
+import firstbackground from "./images/MaskGroup.jpg";
+import StudentUpdate from "./StudentUpdate";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen ">
+      <div className="w-full ">
+        <div className="bg-[#141414] py-5 ">
+          <h1 className="text-3xl font-bold pl-10 text-white ">
+            Madison School District
+          </h1>
+        </div>
+        <img
+          src={firstbackground.src}
+          loading="lazy"
+          className=" bg-red w-full mx-auto h-[500px]"
+          alt="backgroungimg"
+        />
+      </div>
+
+      <div className="w-full mt-6">
+        <div className=" w-[96%] mx-auto pb-10 bg-[#F6F6F6]  ">
+          <div>
+            {" "}
+            <h1 className="text-2xl sm:text-6xl font-bold text-black text-center py-3">
+              Baked Delights <span className="text-[#FE5E55]">Gala</span>
+            </h1>
+          </div>
+          <div className=" justify-center items-center mt-4">
+            <p className="text-2xl text-[#4F4F4F] text-center px-10">
+              Join us for a delicious extravaganza! Our students have <br />{" "}
+              worked hard to prepare an array of baked goods to tantalize <br />
+              your taste buds
+            </p>
+            <p className="text-2xl text-[#4F4F4F] pt-10 text-center px-10">
+              Annual Delights Gala To support the Junior class's
+              <br /> journey to Brazil, come relish our bake sale next
+              <br /> Monday after classes!
+            </p>
+
+            <p className="text-2xl text-[#1D1D1D] pt-10 text-center px-10">
+              Central School Auditorium, 2:30 p.m. - 3:30 p.m.
+            </p>
+            <div className="w-full flex justify-center ">
+              {" "}
+              <button className="bg-[#FE5E55] rounded-lg px-2 py-4 text-sm sm:text-3xl text-white w-[80%] sm:w-[50%] mt-6">
+                <p className="text-center">
+                  Can't make it to the gala? Support Online!
+                </p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <StudentUpdate />
+      <Footer />
+    </div>
+  );
+}
